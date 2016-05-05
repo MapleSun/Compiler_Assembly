fa9525402ef4fc078d204c5472489247991b2a0b
MapleSun <maplesyj@gmail.com>
2016-05-05 11:52:01 -0700

    Initial Commit

---
+++ Assembly/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assembly
+//
+//  Created by SYJ on 5/5/16.
+//  Copyright © 2016 SunYijie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
